859b061b69fd45f6ddc0586fb4d23636d4047a79
Luis Raúl <luisraulreyes@gmail.com>
2025-03-11 10:47:40 -0600

    Toast and Automatic navegation

--- app/src/main/java/udelp/edu/mx/agendakotlin/FirstFragment.kt
+++ app/src/main/java/udelp/edu/mx/agendakotlin/FirstFragment.kt
@@ -2,6 +2,7 @@ package udelp.edu.mx.agendakotlin
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View

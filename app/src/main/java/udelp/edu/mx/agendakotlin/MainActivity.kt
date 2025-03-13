52952afaf25661fd53e0b75b2c6a10fe3c4b7744
Luis Raúl <luisraulreyes@gmail.com>
2025-03-12 21:08:55 -0600

    Side Menu

--- app/src/main/java/udelp/edu/mx/agendakotlin/MainActivity.kt
+++ app/src/main/java/udelp/edu/mx/agendakotlin/MainActivity.kt
@@ -9,47 +9,56 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import udelp.edu.mx.agendakotlin.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navDrawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        navDrawer = findViewById<DrawerLayout>(R.id.drawer_layout)
 
-        setSupportActionBar(binding.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
 
+        val toggle = ActionBarDrawerToggle(this,navDrawer, toolbar,
+            R.string.app_name, R.string.app_name)
 
-    }
+        navDrawer.addDrawerListener(toggle)
+        toggle.syncState()
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
         }
+
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.nav_home -> {supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainFragment()).commit()}
+            R.id.nav_tarea -> {supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FirstFragment()).commit()}
+            R.id.nav_contacto -> {supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ContactoFragment()).commit()}
+        }
+        navDrawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
